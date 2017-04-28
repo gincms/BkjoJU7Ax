30076f03fb59c6d3b19c8d0611c97aca224a189b
Latn Black <latn.black@gmail.com>
2017-04-28 10:55:07 +0100

    28/04/2017 09:55:07: Created 'Codeblock' content type

---
+++ data/sys/schema/content-types/codeblock.js
@@ -0,0 +1,50 @@
+
+/**
+ * Codeblock content type
+ */
+module.exports = {
+  "name": "codeblock",
+  "label": "Codeblock",
+  "fields": {
+    "title": {
+      "name": "title",
+      "label": "Title",
+      "type": "text",
+      "placeholder": "Title"
+    },
+    "slug": {
+      "name": "slug",
+      "label": "Slug",
+      "type": "slug",
+      "placeholder": "Slug"
+    },
+    "language": {
+      "name": "language",
+      "label": "Language",
+      "type": "select",
+      "placeholder": "Language",
+      "options": [
+        {
+          "label": "Javascript",
+          "value": "javascript"
+        },
+        {
+          "label": "Bash",
+          "value": "bash"
+        },
+        {
+          "label": "HTML",
+          "value": "html"
+        },
+        {
+          "label": "Sass",
+          "value": "sass"
+        },
+        {
+          "label": "CSS",
+          "value": "css"
+        }
+      ]
+    }
+  }
+}
