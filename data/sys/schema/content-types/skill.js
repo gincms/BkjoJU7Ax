ac0c330061d7436f180280e4a9479bf01eb02acc
Latn Black <latn.black@gmail.com>
2017-08-02 11:40:13 +0100

    02/08/2017 10:40:13: Created 'Skill' content type

---
+++ data/sys/schema/content-types/skill.js
@@ -0,0 +1,71 @@
+
+/**
+ * Skill content type
+ */
+module.exports = {
+  "name": "skill",
+  "label": "Skill",
+  "icon": "university",
+  "description": "",
+  "timestamps": true,
+  "publishable": false,
+  "fields": {
+    "name": {
+      "name": "name",
+      "label": "Name",
+      "type": "text",
+      "placeholder": "Name"
+    },
+    "rating": {
+      "name": "rating",
+      "label": "Rating",
+      "type": "select",
+      "placeholder": "Rating",
+      "options": [
+        {
+          "label": "0",
+          "value": "0"
+        },
+        {
+          "label": "1",
+          "value": "1"
+        },
+        {
+          "label": "2",
+          "value": "2"
+        },
+        {
+          "label": "3",
+          "value": "3"
+        },
+        {
+          "label": "4",
+          "value": "4"
+        },
+        {
+          "label": "5",
+          "value": "5"
+        }
+      ]
+    },
+    "summary": {
+      "name": "summary",
+      "label": "Summary",
+      "type": "textarea",
+      "placeholder": "Summary",
+      "editor": "plain"
+    },
+    "created": {
+      "name": "created",
+      "label": "Created",
+      "type": "datetime",
+      "readonly": true
+    },
+    "updated": {
+      "name": "updated",
+      "label": "Updated",
+      "type": "datetime",
+      "readonly": true
+    }
+  }
+}
