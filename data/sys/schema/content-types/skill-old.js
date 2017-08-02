00207f7e98aed6a4ba2e47f31d8b1cf05cf3a478
Latn Black <latn.black@gmail.com>
2017-08-02 16:36:25 +0100

    02/08/2017 15:36:25: Updated 'Skill old' content type

---
+++ data/sys/schema/content-types/skill-old.js
@@ -0,0 +1,79 @@
+
+/**
+ * Skill old content type
+ */
+module.exports = {
+  "name": "skill-old",
+  "label": "Skill old",
+  "plural": "Skills",
+  "icon": "university",
+  "description": "Skills and experience I've acquired.",
+  "timestamps": true,
+  "publishable": false,
+  "fields": {
+    "name": {
+      "name": "name",
+      "label": "Name",
+      "type": "text",
+      "placeholder": "Name"
+    },
+    "rating": {
+      "name": "rating",
+      "label": "Rating",
+      "type": "select",
+      "placeholder": "Rating",
+      "options": [
+        {
+          "label": "0",
+          "value": "0"
+        },
+        {
+          "label": "1",
+          "value": "1"
+        },
+        {
+          "label": "2",
+          "value": "2"
+        },
+        {
+          "label": "3",
+          "value": "3"
+        },
+        {
+          "label": "4",
+          "value": "4"
+        },
+        {
+          "label": "5",
+          "value": "5"
+        }
+      ]
+    },
+    "summary": {
+      "name": "summary",
+      "label": "Summary",
+      "type": "textarea",
+      "placeholder": "Summary",
+      "editor": "markdown"
+    },
+    "created": {
+      "name": "created",
+      "label": "Created",
+      "type": "datetime",
+      "readonly": true
+    },
+    "updated": {
+      "name": "updated",
+      "label": "Updated",
+      "type": "datetime",
+      "readonly": true
+    },
+    "": {
+      "name": "",
+      "label": "",
+      "type": "radio",
+      "placeholder": "",
+      "options": ""
+    }
+  }
+}
